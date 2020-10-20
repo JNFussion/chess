4ca0060b7245d8d40dc5f7aca9da9a43acc91447
JNFussion <juan.jose.gutierrez.99@gmail.com>
2020-10-20 20:24:58 +0200

    add 2 methods

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,7 +1,10 @@
+require_relative 'square'
+require_relative 'pawn'
+
 class Chess
-
+  attr_reader :board
   def initialize
-
+    @board = self.initialize_board
   end
 
   def menu
@@ -11,4 +14,51 @@ class Chess
     puts "3. Load game."
 
   end
+
+  def initialize_board
+    board  = Hash.new
+    letters = ['A', 'B', 'C', 'D', 'E' , 'F', 'G', 'H']
+    numbers = (1..8).to_a.reverse
+    row = 8
+    numbers.each do |number|
+      column = 1
+      letters.each do |letter|
+        key = letter + number.to_s
+        board[key] = Square.new([row, column])
+        column += 1
+      end
+      row -= 1
+    end
+
+    initialize_pawns('black', letters, board)
+
+    initialize_pawns('white', letters, board)
+
+    board
+  end
+
+  def initialize_pawns(color, letters, board)
+    letters.each do |letter|
+      square = color == 'white' ? board[letter + '2'] : board[letter + '7']
+      square.piece = Pawn.new(color, square.position) 
+    end
+  end
+
+  def draw_board
+    index = 1
+    @board.each_value do |value|
+       if value.piece.nil?
+        print "."
+      else
+        print value.piece.SYMBOL
+      end
+      puts if index%8 == 0
+      index += 1
+    end
+
+  end
 end
+
+chess = Chess.new
+chess.draw_board
+p chess.board
