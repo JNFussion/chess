0a8f841f29f7496a64799eca5e6e7be444d7de47
JNFussion <juan.jose.gutierrez.99@gmail.com>
2020-10-22 13:55:34 +0200

    possibles movements added

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -3,23 +3,54 @@ require_relative 'piece'
 class Pawn < Piece
 
   @@SYMBOLS = {white: "\u2659", black: "\u265F" }
-  @@MOVEMENT = [[1,0], [2, 0], [-1, 0], [-2, 0], [1,1], [1,-1], [-1,1], [-1,-1]]
+  @@MOVEMENT_WHITE = [[-1,0], [-2, 0], [-1,-1], [-1,1]]
+  @@MOVEMENT_BLACK = [[1,0],[2,0],[1,-1],[1,1]]
   
-  def initialize(color, initial_position)
+  def initialize(color, initial_position, board = nil)
     super(color, initial_position)
     @SYMBOL = @@SYMBOLS[color.to_sym]
-
+    @possible_movement = generate_possible_movement(board)
   end
 
-  def possible_movement(board)
-    pos_mov = @@MOVEMENT
+  def generate_possible_movement(board)
+    return possible_movement_white(board) if self.COLOR == 'white'
+    return possible_movement_black(board) if self.COLOR == 'black'
+  end
+
+  def possible_movement_white(board)
+    pos_mov = @@MOVEMENT_WHITE
+
+    # Delete the positions out of the board 
     pos_mov.delete_if {|coor| !valid_mode?(self.position[0] + coor[0],self.position[1] + coor[1] )}
+    # Delete the 2 square initial movement
+    pos_mov.delete([-2,0]) if self.position != self.INITIAL_POSITION
+    # Delete move forward if there is a piece
+    pos_mov.delete([-1, 0]) unless board[self.position[0] - 1][self.position[1]].piece.nil?
     
-    # delete de 2 squre move
-    pos_mov.delete([2,0]) if self.position != self.initial_position
-    pos_mov.delete([-2,0]) if self.position != self.initial_position
-
-    @@possible_movement = pos_mov
+    # Delete move diagonal if there aren't black pieces
+    pos_mov.delete([-1,-1]) if valid_mode?(self.position[0] - 1, self.position[1] - 1) && (board[self.position[0] - 1][self.position[1] - 1].piece.nil? || board[self.position[0] - 1][self.position[1] - 1].piece.COLOR == 'black')
+    pos_mov.delete([-1,1]) if valid_mode?(self.position[0] - 1, self.position[1] + 1) && (board[self.position[0] - 1][self.position[1] + 1].piece.nil? || board[self.position[0] - 1][self.position[1] + 1].piece.COLOR == 'black')
+    p self
+    p pos_mov
+    pos_mov
   end
+
+  def possible_movement_black(board)
+    pos_mov = @@MOVEMENT_BLACK
+    p self
+    # Delete the positions out of the board 
+    pos_mov.delete_if {|coor| !valid_mode?(self.position[0] + coor[0], self.position[1] + coor[1] )}
+    # Delete the 2 square initial movement
+    pos_mov.delete([2,0]) if self.position != self.INITIAL_POSITION
+    # Delete move forward if there is a piece
+    pos_mov.delete([1, 0]) unless board[self.position[0] + 1][self.position[1]].piece.nil?
+    
+    # Delete move diagonal if there aren't black pieces
+    pos_mov.delete([1,-1]) if valid_mode?(self.position[0] + 1, self.position[1] - 1) && (board[self.position[0] + 1][self.position[1] - 1].piece.nil? || board[self.position[0] + 1][self.position[1] - 1].piece.COLOR == 'white')
+    pos_mov.delete([1,1]) if valid_mode?(self.position[0] + 1, self.position[1] + 1) && (board[self.position[0] + 1][self.position[1] + 1].piece.nil? || board[self.position[0] + 1][self.position[1] + 1].piece.COLOR == 'white')
+    p pos_mov
+    pos_mov
+  end
+
 
 end
