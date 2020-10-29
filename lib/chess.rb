91a3620f9ac0d1a09d15aa27eb4e386a609c04e7
JNFussion <juan.jose.gutierrez.99@gmail.com>
2020-10-29 20:19:02 +0100

    add play method and related method to make it work

--- lib/chess.rb
+++ lib/chess.rb
@@ -12,21 +12,110 @@ require 'pry'
 
 class Chess
   include Factory
+  attr_accessor :check_status
   attr_reader :board
   def initialize(full = true)
     @board = self.generate_full_board(full)
+    @check_status = false
+    @notation_king = ''
+  end
+
+  def game
+
+
+
+
+
+
+
   end
 
   def menu
-    puts "MENU"
+    puts "MENU:"
     puts "1. Play game."
     puts "2. Save game."
     puts "3. Load game."
 
   end
 
+  def play
+
+    info()
+
+    loop do
+      arr_input = []
+      draw_board()
+      loop do
+        puts '[WHITE] My move is: '
+        input = gets.chomp
+        input.upcase!
+        arr_input = input.split(' ')
+        break if correct_input?(arr_input) && move_piece?(arr_input[0], arr_input[1])
+
+        puts 'Incorrect move, try again..'
+      end
+      move_piece(arr_input[0], arr_input[1])
+      draw_board()
+
+      if get_king('black').checkmate?(get_pieces_by_color('white'))
+        puts '*' * 10
+        puts 'WHITE WON!'
+        puts '*' * 10
+        return
+      elsif get_king('black').stalemate?(get_pieces_by_color('white'))
+        puts '*' * 10
+        puts 'DRAW'
+        puts '*' * 10
+        return
+      elsif get_king('black').check?(get_pieces_by_color('white'))
+        @notation_king = get_notation(get_king('black'))
+      end
+
+      loop do
+        puts '[BLACK] My move is: '
+        input = gets.chomp
+        input.upcase!
+        arr_input = input.split(' ')
+        break if correct_input?(arr_input) && move_piece?(arr_input[0], arr_input[1])
+
+        puts 'Incorrect move, try again..'
+      end
+      
+      move_piece(arr_input[0], arr_input[1])
+
+      if get_king('white').checkmate?(get_pieces_by_color('black'))
+        puts '*' * 10
+        puts 'BLACK WON!'
+        puts '*' * 10
+        return
+      elsif get_king('white').stalemate?(get_pieces_by_color('black'))
+        puts '*' * 10
+        puts 'DRAW'
+        puts '*' * 10
+        return
+      elsif get_king('white').check?(get_pieces_by_color('black'))
+        @notation_king = get_notation(get_king('white'))
+      end
+    end
+  end
+# DONT LET KING MOVE TO X POSITION IF THAT POSITION IS IN CHECK"
+  def info
+    puts 'Info:'
+    puts '1. To move a piece use the following anotation: "Initial position of piece to move" "Target position", ei: "B8 C6"'
+    puts '2. Type "DRAW" to propose a draw.'
+    puts '2. Type "EXIT" to go back to menu.'
+    puts
+    puts 'Type to continue...'
+    gets
+  end
+
+  def move_piece?(src, dest)
+    get_piece_by_notation(src).possible_movement?(get_indeces(dest))
+  end
+
   def move_piece(src, dest)
-    return unless get_piece_by_notation(src).possible_movement?(get_indeces(dest))
+    return unless move_piece?(src, dest)
+
     src_indeces = get_indeces(src)
     dest_indeces = get_indeces(dest)
 
@@ -59,6 +148,18 @@ class Chess
 
   end
 
+  def get_king(color)
+    
+    board.each do |row|
+      row.each do |square|
+        next if square.piece.nil?
+        return square.piece if square.piece.COLOR == color && square.piece.instance_of?(King)
+      end
+    end
+
+    nil
+  end
+
   def get_pieces_by_color(color)
     pieces = []
     board.each do |row|
@@ -77,12 +178,28 @@ class Chess
     end
   end
 
+  def get_notation(piece)
+    board.each do |row|
+      row.each do |square|
+         return square.NOTATION if square.piece == piece
+      end
+    end
+  end
+
   def get_piece_by_notation(notation)
     board.each do |row|
       row.each do |square|
          return square.piece if square.NOTATION == notation
       end
     end
+  end
+
+  def correct_input?(input)
+    arr = input.split(' ')
+    return false if arr.size != 2
+    return false if arr[0].size != 2 || arr[1].size != 2
+    return arr[0] == @notation_king && arr[1][0].match?(/[A-H]/) && arr[1][1].match?(/[1-8]/) if @check_status
+    arr[0][0].match?(/[A-H]/) && arr[0][1].match?(/[1-8]/) && arr[1][0].match?(/[A-H]/) && arr[1][1].match?(/[1-8]/)
   end
 
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -23,21 +23,20 @@ describe 'Chess' do
     context 'D4 Rook initial board' do
       
       it 'White rook: D4 to D2 return nil' do
-        @chess.board[4][3].piece  = Rook.new('white',[4,3])
+        @chess.board[4][3].piece  = Rook.new('white',[4,3], Movement::ROOK_MOVEMENT)
         @chess.board[4][3].piece.generate_possible_movement(@chess.board)
         expect(@chess.move_piece('D4', 'D2')).to eql(nil)
       end
 
       it 'White rook: D4 to D7 return Rook' do
-        @chess.board[4][3].piece  = Rook.new('white',[4,3])
+        @chess.board[4][3].piece  = Rook.new('white',[4,3], Movement::ROOK_MOVEMENT)
         @chess.board[4][3].piece.generate_possible_movement(@chess.board)
-        
         expect(@chess.move_piece('D4', 'D7')).to eql(@chess.board[1][3].piece)
         
       end
       
       it 'White rook: D4 to B4 return Rook' do
-        @chess.board[4][3].piece  = Rook.new('white',[4,3])
+        @chess.board[4][3].piece  = Rook.new('white',[4,3], Movement::ROOK_MOVEMENT)
         @chess.board[4][3].piece.generate_possible_movement(@chess.board)
         
         expect(@chess.move_piece('D4', 'B4')).to eql(@chess.board[4][1].piece)
@@ -45,31 +44,27 @@ describe 'Chess' do
       end
 
       it 'White rook: D4 to H4 return Rook' do
-        @chess.board[4][3].piece  = Rook.new('white',[4,3])
+        @chess.board[4][3].piece  = Rook.new('white',[4,3], Movement::ROOK_MOVEMENT)
         @chess.board[4][3].piece.generate_possible_movement(@chess.board)
-        
         expect(@chess.move_piece('D4', 'H4')).to eql(@chess.board[4][7].piece)
         
       end
-      # AAAAAAAAAAAAAAAAAAAA
+
       it 'Black rook: D4 to D2 return nil' do
-        @chess.board[4][3].piece  = Rook.new('black',[4,3])
+        @chess.board[4][3].piece  = Rook.new('black',[4,3], Movement::ROOK_MOVEMENT)
         @chess.board[4][3].piece.generate_possible_movement(@chess.board)
-        
         expect(@chess.move_piece('D4', 'D2')).to eql(@chess.board[6][3].piece)
-        
       end
 
       it 'Black rook: D4 to D7 return Rook' do
-        @chess.board[4][3].piece  = Rook.new('black',[4,3])
+        @chess.board[4][3].piece  = Rook.new('black',[4,3], Movement::ROOK_MOVEMENT)
         @chess.board[4][3].piece.generate_possible_movement(@chess.board)
-        
         expect(@chess.move_piece('D4', 'D7')).to eql(nil)
         
       end
       
       it 'Black rook: D4 to B4 return Rook' do
-        @chess.board[4][3].piece  = Rook.new('black',[4,3])
+        @chess.board[4][3].piece  = Rook.new('black',[4,3], Movement::ROOK_MOVEMENT)
         @chess.board[4][3].piece.generate_possible_movement(@chess.board)
         
         expect(@chess.move_piece('D4', 'B4')).to eql(@chess.board[4][1].piece)
@@ -77,12 +72,58 @@ describe 'Chess' do
       end
 
       it 'Black rook: D4 to H4 return Rook' do
-        @chess.board[4][3].piece  = Rook.new('black',[4,3])
+        @chess.board[4][3].piece  = Rook.new('black',[4,3], Movement::ROOK_MOVEMENT)
         @chess.board[4][3].piece.generate_possible_movement(@chess.board)
         
         expect(@chess.move_piece('D4', 'H4')).to eql(@chess.board[4][7].piece)
         
       end
     end
+  end
+
+  describe '#correct_input?' do
+    it 'Input: "asd" return false' do
+      expect(@chess.correct_input?('asd')).to eql(false)
+    end
+    it 'Input: "123456789" return false' do
+      expect(@chess.correct_input?('123456789')).to eql(false)
+    end
+    it 'Input: "as 12" return false' do
+      expect(@chess.correct_input?('asd 123')).to eql(false)
+    end
+    it 'Input: "Z9 G1" return true' do
+      expect(@chess.correct_input?('A2 A1')).to eql(true)
+    end
+    it 'Input: "A2A1" return false' do
+      expect(@chess.correct_input?('A2A1')).to eql(false)
+    end
+    it 'Input: "A2 A1" return true' do
+      expect(@chess.correct_input?('A2 A1')).to eql(true)
+    end
+
+    it 'Input: "A2 C8" return true' do
+      expect(@chess.correct_input?('A2 C8')).to eql(true)
+    end
+  end
+
+  describe '#get_king' do
+    it 'Get white king' do
+      expect(@chess.get_king('white')).to eql(@chess.board[7][4].piece)
+    end
+
+    it 'Get black King' do
+      expect(@chess.get_king('black')).to eql(@chess.board[0][4].piece)
+    end
+
+    it 'There is no white king' do
+      empty_chess = Chess.new(false)
+      expect(empty_chess.get_king('white')).to eql(nil)
+    end
+
+    it 'There is no white king' do
+      empty_chess = Chess.new(false)
+      expect(empty_chess.get_king('black')).to eql(nil)
+    end
+    
   end
 end
